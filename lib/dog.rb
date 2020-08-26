1b3dd4c2cac6b8396ba40a806b089a1da80c3683
Zackary Cammack <zack.cammack@gmail.com>
2020-08-26 00:27:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,17 @@
 class Dog
-  def initialize(name)
-    @name = name
+  
+  def name=(dogs_name)
+    @name = dogs_name
   end
+  
   def name
-    @name
+    @name 
   end
-  def name=(new_name)
-    @name = new_name
+  def breed=(breed)
+    @breed = breed
   end
+  
+  def breed
+    @breed
+  end 
 end
