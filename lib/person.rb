b10774258d6797fdca652a822878d7f1bd766bc3
Zackary Cammack <zack.cammack@gmail.com>
2020-08-26 00:29:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,18 @@
 class Person
 
-  def name(person_name)
+  def name=(person_name)
     @name = person_name
   end
 
   def name=(new_person_name)
-    @name = new_person_name
+    @name
   end
 
-  def job(person_job)
+  def job=(person_job)
     @job = person_job
   end
 
   def job=(new_person_job)
-    @job = new_person_job
+    @job
   end
 end
