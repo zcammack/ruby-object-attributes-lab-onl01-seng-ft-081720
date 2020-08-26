f24951ce5670944d68a5f25206b7c3358cfb8d5e
Zackary Cammack <zack.cammack@gmail.com>
2020-08-26 00:14:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,5 +6,14 @@ class Dog
 
   def name=(new_name)
     @this_dog_name = new_name
+  end
+
+  def breed(breed_type)
+    @this_dog_breed = breed_type
+  end
+
+  def breed=(new_breed_type)
+    @this_dog_breed = new_breed_type
+  end
 
 end
